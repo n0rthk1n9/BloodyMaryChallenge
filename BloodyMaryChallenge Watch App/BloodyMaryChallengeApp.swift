a5ecbc9a77f6be5c23f78835eb7ada8cafce357f
Jan Armbrust <jan.armbrust@googlemail.com>
2025-02-03 10:37:06 +0100

    Initial commit

---
+++ BloodyMaryChallenge Watch App/BloodyMaryChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloodyMaryChallengeApp.swift
+//  BloodyMaryChallenge Watch App
+//
+//  Created by Jan Armbrust on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BloodyMaryChallenge_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
